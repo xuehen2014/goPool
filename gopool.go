32953f2c1f9f83e7268b0f6e6e5a4b886d3ee362
xuehen2014 <1242765736@qq.com>
2023-08-07 16:35:23 +0800

    days_002-改进

--- gopool.go
+++ gopool.go
@@ -9,32 +9,43 @@ type Task func()
 * Release 方法会关闭任务队列，并等待所有的 Worker 完成当前的任务
  */
 type GoPool struct {
-	TaskQueue  chan Task
-	MaxWorkers int
-	Workers    []*Worker
+	MaxWorkers  int
+	Workers     []*Worker
+	workerStack []int
 }
 
 func NewGoPool(maxWorkers int) *GoPool {
 	pool := &GoPool{
-		TaskQueue:  make(chan Task),
-		MaxWorkers: maxWorkers,
-		Workers:    make([]*Worker, maxWorkers),
+		MaxWorkers:  maxWorkers,
+		Workers:     make([]*Worker, maxWorkers),
+		workerStack: make([]int, maxWorkers),
 	}
 	for i := 0; i < maxWorkers; i++ {
-		worker := newWorker(pool.TaskQueue)
+		worker := newWorker()
 		pool.Workers[i] = worker
-		worker.start()
+		pool.workerStack[i] = i
+		worker.start(pool, i)
 	}
 	return pool
 }
 
 func (p *GoPool) AddTask(task Task) {
-	p.TaskQueue <- task
+	workerIndex := p.popWorker()
+	p.Workers[workerIndex].TaskQueue <- task
 }
 
 func (p *GoPool) Release() {
-	close(p.TaskQueue)
 	for _, worker := range p.Workers {
-		<-worker.TaskQueue
+		close(worker.TaskQueue)
 	}
+}
+
+func (p *GoPool) popWorker() int {
+	workerIndex := p.workerStack[len(p.workerStack)-1]
+	p.workerStack = p.workerStack[:len(p.workerStack)-1]
+	return workerIndex
+}
+
+func (p *GoPool) pushWorker(workerIndex int) {
+	p.workerStack = append(p.workerStack, workerIndex)
 }
